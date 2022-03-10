0ad14b3ccca9abde54b1d10489de1e3ce4ab322e
GiulioAlberti <g3alberti@gmail.com>
2022-03-10 18:36:11 +0100

    Added test

--- src/test/java/PlayerTests.java
+++ src/test/java/PlayerTests.java
@@ -1,6 +1,8 @@
 import Abstract.*;
 import Structure.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
@@ -21,6 +23,19 @@ public class PlayerTests {
         HumanPlayer.setScanner(new Scanner(fakeScanner));
         Coordinates chosenMove= player.chooseMove(move);
         Coordinates expected=new Coordinates("5c");
+        assertEquals(expected, chosenMove);
+    }
+    @ParameterizedTest
+    @CsvSource({"5, 3, 5b", "6, 4, 7d", "3, 5, 2e"})
+    void greedyPlayerMove(int row, int column, String coordinates){
+        Board board =new Board();
+        board.othelloSetup();
+        Disk disk = new Disk(Status.BLACK);
+        board.setDiskAt(new Coordinates(row,column), Status.WHITE);
+        Move move=new Move(board, disk);
+        GreedyPlayer player =new GreedyPlayer("Greedy", disk);
+        Coordinates chosenMove= player.chooseMove(move);
+        Coordinates expected=new Coordinates(coordinates);
         assertEquals(expected, chosenMove);
     }
 }
